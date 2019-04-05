33a452971225b9b0f5bcf0a8b1e555dd035fb330
KUUBIK <robrad@mail.ru>
2019-04-05 21:56:56 +0600

    com2

--- PyTelega.py
+++ PyTelega.py
@@ -29,7 +29,7 @@ def handle_start_help(message):
                                             'выберите себе ноготочки'
                          )
         user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
-        user_markup.row('/start', '/фоточка')
+        user_markup.row('/start', '/фоточка', '/testing_mode')
         user_markup.row('/видосик', '/аудюшка')
         bot.send_message(message.chat.id,'Дратути', reply_markup=user_markup)
 
@@ -41,44 +41,63 @@ def send_photo(message):
         img = open(directory, 'rb')
         bot.send_message(message.chat.id, 'load photos!')
         bot.send_photo(message.chat.id, img)
+        img.close()
+
+
+@bot.message_handler(commands=['видосик'])
+def send_video(message):
+   if message.text == '/видосик':
+        dir = 'video/1.mp3'
+        video = open(dir, 'rb')
+        bot.send_message(message.chat.id, 'load video!')
+        bot.send_video(message.chat.id, video)
+        video.close()
+
+
+@bot.message_handler(commands=['аудюшка'])
+def send_audio(message):
+    if message.text == '/аудюшка':
+        dir = 'audio/1.mp3'
+        video = open(dir, 'rb')
+        bot.send_message(message.chat.id, 'load video!')
+        bot.send_video(message.chat.id, video)
+        video.close()
 
 
 
-@bot.message_handler(commands=['weather'])
-def weather(message):
-    bot.send_message(message.chat.id, 'Погода билять!')
+
+@bot.message_handler(commands=['testing_mode'])
+def testing_mode(message):
+    bot.send_message(message.chat.id, "Приветствую мы компания занимающаяс продажей воздуха!!!"
+
+                     )
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
+    user_markup.row('1', '2', '3', '4')
+    bot.send_message(message.chat.id,   "1.Почему мы?" + "\n"
+                                        "2.Зачем это вам?" +"\n"
+                                        "3.Чем мы лучше?" + "\n"
+                                        "4.Немного о самой компании", reply_markup=user_markup)
 
 
-@bot.message_handler(func=lambda message: True,commands=['reverse'])
-def reverseMessage(message):
-    bot.send_message(message.chat.id, message.text)
+
+@bot.message_handler(content_types=['text'])
+def command_1(message):
+    if message.text == '1':
+        bot.send_message(message.chat.id, 'congratulations!!')
+    else:
+        echo_msg(message) #тут используем функцию 
 
 
 
 
-@bot.message_handler(func=lambda message: True,commands=['time'])
-def time(message):
-    # listZvonok = ['8:00','9:05' ,'10:10' ,'11:15' ,'12:20' ,'13:25' ,'14:30', '15:35', '16:40', '17:45', '18:50' ]
-
-    listZvonok = ['8:50','9:55' ,'11:10' ,'12:05' ,'13:10' ,'14:15' ,'15:20', '16:25', '17:30', '18:35', '19:40' ]
-    nowHour = datetime.datetime.now()
-    Hours = nowHour.hour
-    # print(Hours)
-    Hours = Hours - 8
-    try:
-        minute = listZvonok[Hours]
-        minute = 'Пара заканчиваться в ' + str(minute)
-        bot.send_message(message.chat.id, minute)
-    except IndexError:
-        bot.send_message(message.chat.id, 'Time is not difined!')
 
 
 @bot.message_handler(func=lambda message: True,content_types=['text'])
 def echo_msg(message):
     bot.send_message(message.chat.id, message.text) # здесь он отправляеть сообщение (What??) на наш телеграмм бот
-    # print(message.text)
+    print(message.text)
     markup = types.ReplyKeyboardRemove(True)
-    bot.send_message(message.chat.id, "Choose one letter:" + str(message.text), reply_markup=markup)
+    bot.send_message(message.chat.id, "Простите я непонял ваш вопрос :" + str(message.text), reply_markup=markup)
 
 
 
